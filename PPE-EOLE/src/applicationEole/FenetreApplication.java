100b10502f0236d1254c11d2a7d78ba7e845fa79
gaetan treuschel <gaetan.treuschel@gmail.com>
2019-12-07 15:15:35 +0100

    Fenetre taille

--- PPE-EOLE/src/applicationEole/FenetreApplication.java
+++ PPE-EOLE/src/applicationEole/FenetreApplication.java
@@ -1,10 +1,32 @@
 package applicationEole;
 
-public class FenetreApplication {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
+
+public class FenetreApplication extends JFrame {
+	private static final long serialVersionUID = 8890778608297071873L;
+	
+	
+	
+	public FenetreApplication() {
+		setBounds(0, 0, 1000, 700);
+		setVisible(true);
+		setLocationRelativeTo(null);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel panel = new JPanel();
+		getContentPane().add(panel, BorderLayout.CENTER);
+		panel.setLayout(new CardLayout(0, 0));
+		revalidate();
+	}
+
+	
+	
 
 	public static void main(String[] args) {
 		
-
+		new FenetreApplication();
 	}
 
 }
